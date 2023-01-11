f10aec5dc824e8d0a31b7e0c5ee538657a0be7e8
abdikarim hussein <abdikarim.hussein557@gmail.com>
2023-01-11 21:20:20 +0000

    minor fixes

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -3,11 +3,9 @@ const { createItem, getAllItems, getItemById, updateItem, deleteItem } = require
 const createQuestion = (req, res) => createItem(res, 'question', req.body)
 
 const getAllQuestions = (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
   return getAllItems(res, 'question')
 }
 const getQuestionByID = (req, res) => {
- res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
   return getItemById(res, 'question', req.params.id)
 }
 const updateQuestion = (req, res) => updateItem(res, 'question', req.body, req.params.id)
